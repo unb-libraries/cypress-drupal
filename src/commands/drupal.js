e2eef84c02055bc06f8e8fb90a9fb7d91360b1b1
Patrick Schilf <pschilf@unb.ca>
2022-09-08 14:33:50 -0300

    Add searchAndSelect command

--- src/commands/drupal.js
+++ src/commands/drupal.js
@@ -14,3 +14,17 @@ Cypress.Commands.add('drupalLoginAs', (userRole) => {
     cy.drupalLogin(user.name, user.pass)
   })
 })
+
+Cypress.Commands.add('drupalSearchAndSelect', {prevSubject: 'element'}, (subject, values, options = {searchTermLength: 3}) => {
+  if (typeof values === 'string') {
+    cy.wrap(subject).drupalSearchAndSelect([values])
+  }
+  else {
+    values.forEach((value, index) => {
+      cy.wrap(subject).type((index > 0 ? ', ' : '') + value.substr(0, options.searchTermLength))
+      cy.get('ul.ui-widget.ui-autocomplete:visible')
+        .contains(value)
+        .click()
+    })
+  }
+})
