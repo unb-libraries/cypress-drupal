760afb16fde70ac7712162571ba28e6c1ae6d75a
Patrick Schilf <pschilf@unb.ca>
2022-11-04 16:27:04 -0300

    Add widget selectors

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const commands = require('./src/commands')
+const selectors = require('./src/selectors')
 
 module.exports = {
   commands,
+  selectors,
 }
---
+++ src/selectors/index.js
@@ -0,0 +1,13 @@
+const input = (name) => `input[name="${name}"]`
+const text = (name) => `text[name="${name}"]`
+const number = input
+const autocomplete = (name) => `input[name^="${name}["]`
+const fileSelect = (name) => `[name^="files[${name}_"]`
+
+module.exports = {
+  'widget:input': input,
+  'widget:text': text,
+  'widget:number': number,
+  'widget:autocomplete': autocomplete,
+  'widget:fileselect': fileSelect,
+}
