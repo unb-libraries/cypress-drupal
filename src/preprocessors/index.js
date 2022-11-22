1c3faadf509dcd3cfd51d6fe77f6aeab9bd19258
Patrick Schilf <pschilf@unb.ca>
2022-11-22 15:43:14 -0400

    Add yaml preprocessor support for workflows

--- src/preprocessors/index.js
+++ src/preprocessors/index.js
@@ -1,26 +1,31 @@
 const YAML = require('yaml')
 
-class DrupalPreprocessor {
+class YamlWorkflowPreprocessor {
   applies(filePath) {
     return filePath.match(/.*\.yml/)
   }
 
   get extname() {
-    return '.drupal.cy.js'
+    return '.yml.js'
   }
 
   transform(spec) {
     const yaml = YAML.parse(spec)
+    if (!yaml.workflow) {
+      throw new Error('Could not find a workflow ID.')
+    }
+
+    const workflowId = Object.keys(yaml.workflow)[0]
+    const workflowParams = JSON.stringify(yaml.workflow[workflowId])
+
     return `
       describe('${yaml.title}', () => {
-        it('should work', () => {
-          expect(true).to.be.true
-        })
+        Cypress.Workflows.run('${workflowId}', ${workflowParams})
       })
     `
   }
 }
 
 module.exports = {
-  drupal: new DrupalPreprocessor(),
+  drupal: new YamlWorkflowPreprocessor(),
 }
